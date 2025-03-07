c6ba9f1565c4b7fae4e9f62fc190d580f16939e0
Meghanareddy917 <bethimeghanareddy@gmail.com>
2025-03-07 11:51:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to change the text of the paragraph when the button is clicked
+function changeText() {
+    const message = document.getElementById('message');
+    message.textContent = 'You clicked the button! Great job!';
+}
